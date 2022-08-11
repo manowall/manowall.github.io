33893cb6952aefa0936f69f7403a6da9fda82261
manowall <79204559+manowall@users.noreply.github.com>
2022-08-10 19:49:59 -0500

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,87 @@
+var botonEncriptar = document.querySelector(".encriptar");
+var botonDesencriptar = document.querySelector(".desencriptar");
+var muneco = document.querySelector(".contenedorMuneco");
+var h3 = document.querySelector(".tituloMuneco");
+var parrafo = document.querySelector(".parrafoMuneco");
+var resultado = document.querySelector(".textoResultado");
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+
+function encriptar(){
+    ocultar();
+    var area = recuperarTexto();
+    resultado.textContent = encriptarTexto(area);
+}
+
+function desencriptar(){
+    ocultar();
+    var area = recuperarTexto();
+    resultado.textContent = desencriptarTexto(area);
+}
+
+function recuperarTexto(){
+    var area = document.querySelector(".area");
+    return area.value;
+}
+
+function ocultar(){
+    muneco.classList.add("ocultar");
+    h3.classList.add("ocultar");
+    parrafo.classList.add("ocultar"); 
+}
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai";
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter";
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes";
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober";
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat";
+        }else{
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+    return textoFinal;
+}
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a";
+            i++;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e";
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i";
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o";
+            i = i+3;
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u";
+            i = i+3;
+        }else{
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+    return textoFinal;
+}
